491f28e6c032a96d40a025ae4345f1a9928365a5
Gabriel Matos de Lima <gabriel.lima@ICTS.BR>
2020-08-10 18:21:22 -0400

    correctin fix

--- src/App.js
+++ src/App.js
@@ -14,9 +14,15 @@ export default function App() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    api.get("repositories").then((response) => {
-      setRepositories(response.data);
-    });
+    api
+      .get("repositories")
+      .then((response) => {
+        console.log(response.data);
+        setRepositories(response.data);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
   }, []);
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
@@ -51,7 +57,7 @@ export default function App() {
                 <Text
                   style={styles.likeText}
                   // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-                  testID={`repository-likes-1`}
+                  testID={`repository-likes-${repository.id}`}
                 >
                   {repository.likes}
                 </Text>
@@ -61,7 +67,7 @@ export default function App() {
                 style={styles.button}
                 onPress={() => handleLikeRepository(repository.id)}
                 // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-                testID={`like-button-1`}
+                testID={`like-button-${repository.id}`}
               >
                 <Text style={styles.buttonText}>Curtir</Text>
               </TouchableOpacity>
